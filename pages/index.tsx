435d31f6b92c8bdd6aa69b222f4db7bb28208ce1
Hunter Caron <huntercaron@me.com>
2020-06-07 17:04:01 +0200

    Moving to normal css

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import {
     createURLParamString,
     fetchShortenedUrl,
 } from "../utils"
-import { GlobalStyle } from "../utils/globalStyle"
 
 function LinkCreator(props) {
     const { children, url, secondary, areLinksValid, setAreLinksValid } = props
@@ -55,42 +54,6 @@ function LinkCreator(props) {
                     {loading ? <h3>Creating Link…</h3> : children}
                 </div>
             )}
-
-            <style jsx>{`
-                .link-container {
-                    margin-bottom: var(--space);
-                    text-align: center;
-                    height: calc(var(--space) * 2.5);
-                    border-color: ${secondary ? "var(--gray)" : "#000000"};
-                    display: flex;
-                }
-
-                .button {
-                    width: 100%;
-                    height: 100%;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    cursor: pointer;
-                }
-
-                .button:hover {
-                    background-color: rgba(0, 0, 0, 0.033);
-                }
-
-                .link {
-                    flex: 1;
-                    height: 100%;
-                }
-
-                .link input {
-                    width: 100%;
-                    height: 100%;
-                    margin: 0;
-                    flex: 1;
-                    border: 0;
-                }
-            `}</style>
         </div>
     )
 }
@@ -135,7 +98,6 @@ export default function Home() {
                 <h4>To</h4>
                 <input
                     type="text"
-                    className="box"
                     placeholder="Email Address"
                     value={to}
                     onChange={(e) => setTo(e.target.value)}
@@ -143,14 +105,12 @@ export default function Home() {
                 <h4>Subject</h4>
                 <input
                     type="text"
-                    className="box"
                     placeholder="Subject"
                     value={subject}
                     onChange={(e) => setSubject(e.target.value)}
                 />
                 <h4>Body</h4>
                 <textarea
-                    className="box"
                     placeholder="Write your email here…"
                     value={body}
                     onChange={(e) => setBody(e.target.value)}
@@ -167,13 +127,6 @@ export default function Home() {
 
                 <div className="divider" />
 
-                <h4>
-                    Email Link{" "}
-                    <span className="secondary">
-                        A link that auto-composes the email
-                    </span>
-                </h4>
-
                 <LinkCreator
                     url={sendUrl}
                     areLinksValid={areLinksValid}
@@ -183,135 +136,29 @@ export default function Home() {
                         <span role="img" aria-label="email">
                             ✉️
                         </span>{" "}
-                        Create Link
+                        Create link to email
                     </h3>
                 </LinkCreator>
-
                 <h4>
-                    Template Link{" "}
+                    Email Link{" "}
                     <span className="secondary">
-                        A link to a copy of this template
+                        A link that auto-composes the email
                     </span>
                 </h4>
-                <LinkCreator
-                    url={templateUrl}
-                    areLinksValid={areLinksValid}
-                    setAreLinksValid={setAreLinksValid}
-                    secondary
-                >
-                    <h3>
-                        <span role="img" aria-label="template">
-                            📝
-                        </span>{" "}
-                        Create Link
-                    </h3>
-                </LinkCreator>
 
                 <footer className="secondary">
-                    currently only supports plain text •{" "}
-                    <a
-                        href="https://github.com/huntercaron/email-template"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        github repo
-                    </a>
+                    <p>
+                        plain text only • saves automatically •{" "}
+                        <a
+                            href="https://github.com/huntercaron/email-template"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            github repo
+                        </a>
+                    </p>
                 </footer>
             </main>
-
-            <style jsx global>{`
-                .container {
-                    --space: 12px;
-                    --gray: rgba(0, 0, 0, 0.4);
-                    --light-gray: rgba(0, 0, 0, 0.12);
-                    padding: var(--space);
-                }
-
-                main {
-                    display: flex;
-                    flex-direction: column;
-                    max-width: 400px;
-                }
-
-                footer {
-                    font-size: 12px;
-                }
-
-                .box {
-                    border: 1px solid #000;
-                    border-radius: 3px;
-                }
-
-                h3 {
-                    font-weight: 500;
-                    margin: 0;
-                    font-size: 14px;
-                    letter-spacing: -0.25px;
-                }
-
-                h4 {
-                    font-size: 12px;
-                    letter-spacing: -0.2px;
-                    margin: 0 0 4px;
-                }
-
-                .divider {
-                    width: 100%;
-                    height: 1px;
-                    flex-shrink: 0;
-                    border-top: 1px solid var(--light-gray);
-                    margin: var(--space) 0;
-                }
-
-                .secondary {
-                    color: var(--gray);
-                    font-weight: 400;
-                }
-
-                .action {
-                    color: #000;
-                    font-size: 12px;
-                    margin-right: 10px;
-                    text-decoration: underline;
-                }
-
-                input,
-                textarea {
-                    width: 100%;
-                    background-color: transparent;
-                    font-size: 13px;
-                    letter-spacing: -0.1px;
-                    padding: 0.5rem;
-                    margin-bottom: var(--space);
-                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI,
-                        Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-                        Helvetica Neue, sans-serif;
-                }
-
-                textarea {
-                    height: 150px;
-                    min-height: 100px;
-                    min-width: 100%;
-                    margin-bottom: 0px;
-                }
-
-                @media (min-width: 350px) {
-                    .container {
-                        --space: 16px;
-                    }
-                }
-
-                @media (min-width: 500px) {
-                    .container {
-                        --space: 20px;
-                    }
-                    textarea {
-                        height: 260px;
-                    }
-                }
-            `}</style>
-
-            <GlobalStyle />
         </div>
     )
 }
