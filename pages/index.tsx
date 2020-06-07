15832e9b4869847428abca09c8ff2c1393530702
Hunter Caron <huntercaron@me.com>
2020-06-07 20:46:43 +0200

    preventing zoom on iOS

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,6 +115,10 @@ export default function Home() {
             <Head>
                 <title>Email Template</title>
                 <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                />
                 <meta name="twitter:title" content="Email Template" />
                 <meta
                     name="twitter:description"
